502dc0675de72c3040180000abbf08ac5b63a735
Pradip Kumar Jaiswal <pradip.k.jaiswal@gmail.com>
2017-05-02 11:33:48 +0530

    JAVASCRIPT(I) Assignments session-5

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// Below code about displaying properties Using Object Literal Notation in console.
+var person = {
+    name : "John",
+    age : 30,
+    dob : "01-jan-1988",
+    pob : "India",
+};
+console.log("%c My Name is " + person.name + " I am " + person.age + " years old, & born on " 
+            + person.dob + " in " + person.pob + ".", 'background: #4b4b4b; color: white;');
